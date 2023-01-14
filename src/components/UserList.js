b9804330ebc8374cf469f419d009235a27420566
Fernando <fernando.vallejos67@gmail.com>
2023-01-13 21:39:14 -0300

    cambios en response y list

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,49 +3,78 @@ import { useEffect, useState, useRef } from "react";
 import { db } from "../firebase";
 import { Link } from "react-router-dom";
 
-function UserList(){
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const firstLoad = useRef(true);
+function UserList() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const firstLoad = useRef(true);
 
-    useEffect(() => {
-        setLoading(true);
-        const fetchData = async () => {
-           let data = []
-            try {
-                const querySnapshot = await getDocs(collection(db, "users"));
-                querySnapshot.forEach((doc) => {
-                    data.push({id:doc.id, name: doc.data().full_name})
-                  });
-            if (data.length > 0) {
-                setData( data);
-                setLoading(false);
-              } else {
-                setData(null);
-            setLoading(false);
-              }
-          } catch (e) {
-            setData(null);
-            setLoading(false);
-          }
-        };
-    
-        firstLoad.current && fetchData();
-        firstLoad.current = false
-      }, []);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      let data = [];
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          data.push({ id: doc.id, name: doc.data().full_name });
+        });
+        if (data.length > 0) {
+          setData(data);
+          setLoading(false);
+        } else {
+          setData(null);
+          setLoading(false);
+        }
+      } catch (e) {
+        setData(null);
+        setLoading(false);
+      }
+    };
 
+    firstLoad.current && fetchData();
+    firstLoad.current = false;
+  }, []);
 
-      if (loading) return <h2>Cargando...</h2>;
-
-      if (!loading && !data) return <h2>No hay datos</h2>;
-
-return (
+  if (loading)
+    return (
+      <div className="response">
+        <div className="container">
+          <article className="message is-info">
+            <div className="message-header">Cargando...</div>
+          </article>
+        </div>
+      </div>
+    );
+  if (!loading && !data)
+    return (
+      <div className="response">
+        <div className="container">
+          <article className="message is-info">
+            <div className="message-header">Respuesta</div>
+            <div className="message-body">No hay datos</div>
+          </article>
+        </div>
+      </div>
+    );
+  return (
     <div>
-{data.map(item => {
-    return <div className="field" key={item.id}><Link to={`/user/${item.id}`}>{item.name}</Link></div>
-})}
+      <div className="response">
+        <div className="container">
+          <article className="message is-info">
+            <div className="message-header">Datos de usuarios</div>
+            {data.map((item) => {
+              return (
+                <div key={item.id}>
+                  <Link className="label" to={`/user/${item.id}`}>
+                    {item.name}
+                  </Link>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </div>
     </div>
-)
+  );
 }
 
-export default UserList
+export default UserList;
